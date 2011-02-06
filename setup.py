f37d21005b917f3f8b7f978e12dfa6b15e814625
shimizukawa <devnull@localhost>
2011-02-06 16:54:06 +0900

    A sphinx theme for generate HTML Slide presentation.

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os, sys
+
+version = '0.0.1'
+long_description = \
+        open(os.path.join("src","README.txt")).read()
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup",
+]
+
+setup(
+     name='sphinxcontrib-themes-htmlslide',
+     version=version,
+     description='A sphinx theme for generate HTML Slide presentation.',
+     long_description=long_description,
+     classifiers=classifiers,
+     keywords=['sphinx', 'reStructuredText', 'presentation'],
+     author='Takayuki SHIMIZUKAWA',
+     author_email='shimizukawa at gmail dot com',
+     url='http://bitbucket.org/shimizukawa/sphinxcontrib-themes-htmlslide',
+     license='MIT',
+     packages=find_packages('src'),
+     package_dir={'': 'src'},
+     package_data = {'': ['buildout.cfg']},
+     include_package_data=True,
+     install_requires=[
+        'setuptools',
+        'docutils',
+        'sphinx',
+        'sphinxcontrib-themes-core',
+     ],
+     entry_points="""
+        [sphinx_themes]
+        path = sphinxcontrib.themes.htmlslide:template_path
+     """,
+     zip_safe=False,
+)
+
---
+++ src/README.txt
@@ -0,0 +1,51 @@
+HTML Slide style presetation theme for Sphinx.
+
+Features
+========
+* provide ``htmlslide`` theme for render presentation.
+
+
+Setup
+=====
+Make environment with easy_install::
+
+    $ easy_install sphinxcontrib-themes-htmlslide
+
+
+Convert Usage
+==============
+setup conf.py with::
+
+    extensions = ['sphinxcontrib.themes.core']
+    html_theme = 'htmlslide'
+
+and run::
+
+    $ make html
+
+
+Requirements
+============
+* sphinx 1.0.x or later.
+* sphinxcontrib-themes-core
+
+
+Presentation Environments
+==========================
+* HTML5 ready browsers (Chrome, Safari)
+* Non HTML5 ready browsers only show slide (no-animation)
+
+
+License
+=======
+Licensed under the `MIT license <http://www.opensource.org/licenses/mit-license.php>`_ .
+See the LICENSE file for specific terms.
+
+
+History
+=======
+
+0.1 (unreleased)
+-----------------
+* first release
+
